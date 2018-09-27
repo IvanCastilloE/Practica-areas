3e3af95af74960573ad4bee6389af51c5d534089
Angel Ivan Castillo <angel_xayan06@hotmail.com>
2018-09-27 08:47:21 -0700

    Se pone mas chido

--- PracticaAreas/MainWindow.xaml.cs
+++ PracticaAreas/MainWindow.xaml.cs
@@ -27,28 +27,80 @@ namespace PracticaAreas
 
         private void botonRectangulo_Click(object sender, RoutedEventArgs e)
         {
-            float area = float.Parse(alturaRectangulo.Text) * float.Parse(baseRectangulo.Text);
-            areaRectangulo.Text = area.ToString() + " m";
+            /*float area = float.Parse(alturaRectangulo.Text) * float.Parse(baseRectangulo.Text);
+            areaRectangulo.Text = area.ToString() + " m";*/
         }
 
         private void botonTriangulo_Click(object sender, RoutedEventArgs e)
         {
-            float area = float.Parse(alturaTriangulo.Text) * float.Parse(baseTriangulo.Text) / 2;
-            areaTriangulo.Text = area.ToString() + " m";
+            /*float area = float.Parse(alturaTriangulo.Text) * float.Parse(baseTriangulo.Text) / 2;
+            areaTriangulo.Text = area.ToString() + " m";*/
         }
 
         private void botonCirculo_Click(object sender, RoutedEventArgs e)
         {
-            float pi = Convert.ToSingle(Math.PI);
+            /*float pi = Convert.ToSingle(Math.PI);
             float area = float.Parse(radio.Text) * float.Parse(radio.Text) * pi;
-            areaCirculo.Text = area.ToString() + " m";
+            areaCirculo.Text = area.ToString() + " m";*/
         }
 
         private void botonTrapecio_Click(object sender, RoutedEventArgs e)
         {
+            /*
             float area = (float.Parse(baseMayor.Text) + float.Parse(BaseMenor.Text)) 
                 * float.Parse(alturaTrapecio.Text) / 2;
-            areaTrapecio.Text = area.ToString()+" m";
+            areaTrapecio.Text = area.ToString()+" m";*/
+        }
+
+        private void cbTipoFigura_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            panelConfiguracion.Children.Clear();
+            switch (cbTipoFigura.SelectedIndex)
+            {
+                //rectangulo
+                case 0:
+                    panelConfiguracion.Children.Add(new ControlAreaRectangulo());
+                    break;
+                //triangulo
+                case 1:
+                    panelConfiguracion.Children.Add(new ControlAreaTriangulo());
+                    break;
+                //circulo
+                case 2:
+                    panelConfiguracion.Children.Add(new ControlAreaCirculo());
+                    break;
+                //trapecio
+                case 3:
+                    panelConfiguracion.Children.Add(new ControlAreaTrapecio());
+                    break;
+                //nada
+                default:
+                    break;
+
+            }
+        }
+        private void botonClick_Click(object sender, RoutedEventArgs e)
+        {
+            double area = 0.0;
+            switch (cbTipoFigura.SelectedIndex)
+            {
+                //rectangulo
+                case 0:
+                    break;
+                //triangulo
+                case 1:
+                    break;
+                //circulo
+                case 2:
+                    break;
+                //trapecio
+                case 3:
+                    break;
+                //nada
+                default:
+                    break;
+            }
+            lblResultadoArea.Text = area.ToString();
         }
     }
 }
